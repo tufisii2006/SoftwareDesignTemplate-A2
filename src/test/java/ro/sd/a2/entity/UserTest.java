70923a37891244b2b461b7ba4dec2406f7e89cd3
tufisii2006 <tufisii2006@gmail.com>
2021-09-28 12:06:28 +0300

    sdf

--- src/test/java/ro/sd/a2/entity/UserTest.java
+++ src/test/java/ro/sd/a2/entity/UserTest.java
@@ -1,11 +1,14 @@
 package ro.sd.a2.entity;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.util.logging.Logger;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@RunWith(SpringJUnit4ClassRunner.class)
 class UserTest {
 
   Logger logger = Logger.getLogger(UserTest.class.getName());
