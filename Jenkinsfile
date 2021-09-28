8ef3379e5b9aff00d61056e6a6e59d57fac37875
tufisii2006 <tufisii2006@gmail.com>
2021-09-28 10:00:49 +0300

    Added tests

--- Jenkinsfile
+++ Jenkinsfile
@@ -3,16 +3,22 @@ pipeline {
     agent any
 
     stages {
+
         stage('Build') {
             steps {
                 echo 'Building..'
+                git 'https://github.com/tufisii2006/SoftwareDesignTemplate-A2.git'
+                sh './mvnw clean compile'
             }
         }
+
         stage('Test') {
             steps {
                 echo 'Testing..'
+                sh './mvnw test'
             }
         }
+
         stage('Deploy') {
             steps {
                 echo 'Deploying....'
--- src/test/java/ro/sd/a2/Assignment2SdApplicationTests.java
+++ src/test/java/ro/sd/a2/Assignment2SdApplicationTests.java
@@ -1,13 +1,30 @@
 package ro.sd.a2;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
 
-@SpringBootTest
+import java.util.logging.Logger;
+
+
+@RunWith(MockitoJUnitRunner.class)
 class Assignment2SdApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+  Logger logger = Logger.getLogger(Assignment2SdApplicationTests.class.getName());
+
+  @Test
+  void simpleTest() {
+    logger.info("Running Simple test");
+    int a = 2;
+    Assertions.assertTrue(a + 2 > 3);
+  }
+
+  @Test
+  void anotherSimpleTest() {
+    logger.info("Running another Simple test");
+    int b = 3;
+    Assertions.assertEquals(9, b + 6);
+  }
 
 }
